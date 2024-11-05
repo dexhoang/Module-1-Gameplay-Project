cb6174d20bcacfdc8b21d5349e7eb54eee3d9a78
dexhoang <dhhoang@ucsc.eedu>
2024-11-05 01:34:11 -0800

    Added Danger Message

--- Module 1 Project/Assets/Scripts/WeepingAngelAI.cs
+++ Module 1 Project/Assets/Scripts/WeepingAngelAI.cs
@@ -2,32 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
+
+//code for player detection ai given by Omogonix - https://youtu.be/_e57zSZSOS8?si=jHGpMCsrJpD5911Z 
 
 public class WeepingAngelAI : MonoBehaviour
 {
     public NavMeshAgent ai;
     public Transform player;
-    Vector3 dest;
     public Camera playerCam;
     public float aiSpeed;
+    public float dangerDistance = 10f;
+    public Text dangerMessage;
+    private bool isDisplayingMessage = false;
 
     private void Update()
     {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
+
+        if (GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
         {
-            Plane[] planes = GeometryUtility.CalculateFrustumPlanes(playerCam);
-            
-            if (GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
-            {
-                ai.speed = 0;
-            }
-            if (!GeometryUtility.TestPlanesAABB(planes, this.gameObject.GetComponent<Renderer>().bounds))
-            {
-                ai.speed = aiSpeed;
-                dest = player.position;
-                ai.destination = dest;
-            }
+            ai.speed = 0;
+            ai.SetDestination(transform.position);
+        }
+        else
+        {
+            ai.speed = aiSpeed;
+            ai.destination = player.position;
+        }
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer <= dangerDistance)
+        {
+            dangerMessage.gameObject.SetActive(true);
+        }
+        else
+        {
+            dangerMessage.gameObject.SetActive(false);
         }
     }
 
-
+    private IEnumerator DisplayDanger()
+    {
+        isDisplayingMessage = true;
+        dangerMessage.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        dangerMessage.gameObject.SetActive(false);
+        isDisplayingMessage = false;
+    }
 }
